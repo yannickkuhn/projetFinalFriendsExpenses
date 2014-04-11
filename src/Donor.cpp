e22e4e796895feaff0243c4276cfcb1dd63a8344
yannick <you@example.com>
2014-04-11 08:42:36 +0200

    Commit - creation classe Donor dans la branche testDonorYK

---
+++ src/Donor.cpp
@@ -0,0 +1,18 @@
+/*
+ * Donor.cpp
+ *
+ *  Created on: 11 avr. 2014
+ *      Author: ubuntu
+ */
+
+#include "Donor.hpp"
+
+Donor::Donor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Donor::~Donor() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Donor.hpp
@@ -0,0 +1,17 @@
+/*
+ * Donor.hpp
+ *
+ *  Created on: 11 avr. 2014
+ *      Author: ubuntu
+ */
+
+#ifndef DONOR_HPP_
+#define DONOR_HPP_
+
+class Donor {
+public:
+	Donor();
+	virtual ~Donor();
+};
+
+#endif /* DONOR_HPP_ */
